cfdd443ec00fef5cc7c26c277a6d14ff6da53bc8
RTMoo <toktarkazyramazan@gmail.com>
2025-09-14 10:20:49 +0500

    fix: test serializer

--- backend/src/tests/serializers.py
+++ backend/src/tests/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 
 
 class TestSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    created_at = serializers.DateTimeField()
-    preview = serializers.SerializerMethodField()
+    id = serializers.IntegerField(read_only=True)
+    problem_id = serializers.IntegerField(read_only=True, source="problem.id")
+    created_at = serializers.DateTimeField(read_only=True)
+    preview = serializers.SerializerMethodField(read_only=True)
+    input_file = serializers.FileField(write_only=True)
 
     def get_preview(self, obj):
         max_lines = 5
